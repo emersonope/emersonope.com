f2c56936e7ea81e90ad69a2560d0c5fc42923c46
emersonope <emersonope@gmail.com>
2021-03-11 12:12:27 -0300

    titulo pg

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,12 +9,25 @@ import Degree from './degree';
 const Section1 = styled.div`
   justify-content: center;
   align-itens: center;
+  text-align: center;
+  font-family: times new roman;
+  font-style: italic;
+`;
+
+const Title = styled.h1`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  font-size: 50px;
+  font-weight: bold;
+  color: #04C740;
 `;
 
 const Home = () => {
     return(
       <Section1>
-       
+
+       <Title>Bem-vindo(s)</Title>
+        
         <Introduction />
         <Technologies />
         <Degree />
